c2fb211c3bcba12fba6f81863e6bd2202952dbd3
gebretewodros73 <gebretewodros73@gmail.com>
2023-11-05 02:11:28 +0300

    RedactingFormatter class to accept a list of fields as a constructor argument. Implement the format method to filter values in incoming log records using filter_datum.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,8 +30,10 @@ def filter_datum(fields: List[str], redaction: str,
 if __name__ == "__main__":
     fields = ["password", "date_of_birth"]
     messages = [
-        "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-        "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
+        "name=egg;email=eggmin@eggsample.com;"
+        "password=eggcellent;date_of_birth=12/12/1986;",
+        "name=bob;email=bob@dylan.com;password=bobbycool;"
+        "date_of_birth=03/04/1993;"
     ]
 
     for message in messages:
