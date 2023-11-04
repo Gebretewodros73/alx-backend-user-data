ce8cdf17046e7345723993c558145eb1eaa48605
gebretewodros73 <gebretewodros73@gmail.com>
2023-11-05 02:22:03 +0300

    RedactingFormatter class to accept a list of fields as a constructor argument. Implement the format method to filter values in incoming log records using filter_datum.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,29 @@ Module for filtering log messages.
 
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class. """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log record with redacted fields. """
+        for field in self.fields:
+            record.msg = re.sub(
+                rf"{field}=(.*?){self.SEPARATOR}",
+                f"{field}={self.REDACTION}{self.SEPARATOR}",
+                record.msg
+            )
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -25,16 +48,3 @@ def filter_datum(fields: List[str], redaction: str,
         pattern = rf"{field}=(.*?){separator}"
         message = re.sub(pattern, f"{field}={redaction}{separator}", message)
     return message
-
-
-if __name__ == "__main__":
-    fields = ["password", "date_of_birth"]
-    messages = [
-        "name=egg;email=eggmin@eggsample.com;"
-        "password=eggcellent;date_of_birth=12/12/1986;",
-        "name=bob;email=bob@dylan.com;password=bobbycool;"
-        "date_of_birth=03/04/1993;"
-    ]
-
-    for message in messages:
-        print(filter_datum(fields, 'xxx', message, ';'))
